b4b1254fc5a537ade7e316b0ea84c606a9e648eb
GhostEsso <lilipitaham@gmail.com>
2023-03-13 22:37:23 +0000

    Correct Lighthouse Error

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ menuHamburger.addEventListener('click', () => {
 
 const iconMenu = document.querySelector('.icon-menu');
 const menuLinks = document.querySelector('.links');
+const navLinksList = document.querySelectorAll('.links a');
 
 iconMenu.addEventListener('click', () => {
   menuLinks.classList.toggle('open');
   iconMenu.classList.toggle('open');
+});
+
+navLinksList.forEach((link) => {
+  link.addEventListener('click', () => {
+    menuLinks.classList.remove('open');
+    iconMenu.classList.remove('open');
+    navLinks.classList.remove('mobile-menu');
+  });
 });
