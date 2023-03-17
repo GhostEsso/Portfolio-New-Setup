214208d6fd6559ed124f9d53b0c0079c1b43dfee
GhostEsso <lilipitaham@gmail.com>
2023-03-17 16:15:07 +0000

    Validation form work

--- JS/validation.js
+++ JS/validation.js
@@ -1,37 +1,39 @@
+const form = document.getElementById('form');
+const email = document.getElementById('mail');
 
-
-const form = document.getElementById('#form');
-const email = document.getElementById('#email');
 function error(input, message) {
   const popup = document.createElement('span');
   popup.appendChild(document.createTextNode(message));
   popup.classList.add('error-message');
   input.parentNode.insertBefore(popup, input.nextElementSibling);
-  popup.style.color = 'rgb(255, 0, 0)';
+  popup.style.color = 'rgb(217, 1, 21)';
   popup.style.marginBottom = '5px';
   popup.style.paddingLeft = '5px';
   popup.style.top = `${input.offsetTop + input.offsetHeight}px`;
   popup.style.left = `${input.offsetLeft}px`;
 }
 
-
 function checkInputs() {
   const emailValue = email.value.trim();
 
   if (/[A-Z]/.test(emailValue)) {
-    errorThrow(email, 'Please use lowercase only in email ');
+    error(email, 'Please must be in lowercase only');
   }
 }
 
 form.addEventListener('submit', (e) => {
-  const errorBack = form.querySelectorAll('.error-message');
-  e.preventDefault();
-  errorBack.forEach((errorMessage) => {
-    errorMessage.remove();
-  });
-  checkInputs();
   const newMessage = form.querySelectorAll('.error-message');
-  if (newMessage.length === 0) {
-    form.submit();
+  if (newMessage) {
+    newMessage.forEach((message) => {
+      message.remove();
+    });
+  }
+
+  checkInputs();
+
+  const errorMessage = form.querySelectorAll('.error-message');
+
+  if (errorMessage.length > 0) {
+    e.preventDefault();
   }
 });
