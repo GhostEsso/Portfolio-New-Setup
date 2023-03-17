eb78996b1be3d7b6f7aca4b618d9c72c90c751ec
unknown <tgemechis7@gmail.com>
2023-03-17 18:55:21 +0300

    Update javascript.js

--- JS/validation.js
+++ JS/validation.js
@@ -1,21 +1,37 @@
-const form = document.querySelector('#form');
-const email = document.querySelector('#mail');
-const error = document.querySelector('.error');
+
+
+const form = document.getElementById('#form');
+const email = document.getElementById('#email');
+function error(input, message) {
+  const popup = document.createElement('span');
+  popup.appendChild(document.createTextNode(message));
+  popup.classList.add('error-message');
+  input.parentNode.insertBefore(popup, input.nextElementSibling);
+  popup.style.color = 'rgb(255, 0, 0)';
+  popup.style.marginBottom = '5px';
+  popup.style.paddingLeft = '5px';
+  popup.style.top = `${input.offsetTop + input.offsetHeight}px`;
+  popup.style.left = `${input.offsetLeft}px`;
+}
+
+
+function checkInputs() {
+  const emailValue = email.value.trim();
+
+  if (/[A-Z]/.test(emailValue)) {
+    errorThrow(email, 'Please use lowercase only in email ');
+  }
+}
 
 form.addEventListener('submit', (e) => {
-  const hasUpperCase = /[A-Z]/g.test(email.value);
-  const isMissingAtSign = !email.value.includes('@');
-
-  if (hasUpperCase) {
-    error.textContent = 'Email must not contain uppercase letters. (Not Sent)';
-    e.preventDefault();
-  } else if (isMissingAtSign) {
-    error.textContent = 'The caracter @ is missing from your email.';
-    e.preventDefault();
-  } else {
-    // reset error message
-    error.textContent = '';
-    // allow form submission
-    return true;
+  const errorBack = form.querySelectorAll('.error-message');
+  e.preventDefault();
+  errorBack.forEach((errorMessage) => {
+    errorMessage.remove();
+  });
+  checkInputs();
+  const newMessage = form.querySelectorAll('.error-message');
+  if (newMessage.length === 0) {
+    form.submit();
   }
 });
